5198cdf259d0f13f6a7e82a54053677dcdf1a2a7
mendy hershkop <hers.mm@gmail.com>
2025-10-27 12:35:30 +0200

    create prompt_guess

---
+++ hangman/io.py
@@ -0,0 +1,7 @@
+def prompt_guess() -> str:
+    letter=input("Enter a letter: ")
+    return letter
+def print_status(state: dict) -> None:
+    pass
+def print_result(state: dict) -> None:
+    pass
