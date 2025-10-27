d124cc6f3b169268d492c57f9553de535dc612fb
mendy hershkop <hers.mm@gmail.com>
2025-10-27 11:32:39 +0200

    create apply_guess function

--- hangman/game.py
+++ hangman/game.py
@@ -1,15 +1,17 @@
 def init_state(secret: str, max_tries: int) -> dict:
     for i in range(len(secret)+1):
         display=i*"_"
-    return {"secret":str(secret), "display":list[str](display), "guessed": set[str],"wrong_guesses":int,"max_tries":int(max_tries)}
+    return {"secret":secret, "display":list[str](display), "guessed": set[str],"wrong_guesses":int,"max_tries":int(max_tries)}
 def validate_guess(ch: str, guessed: set[str]) -> tuple[bool, str] :
     if ch in guessed:
         return True ,"Good choice"
     else:
         return False ,"This charter already exist"
-
 def apply_guess(state: dict, ch: str) -> bool:
-    pass
+    if ch in state:
+        return True
+    else:
+        return False
 def is_won(state: dict) -> bool:
     pass
 def is_lost(state: dict) -> bool:
