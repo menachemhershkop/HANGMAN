aa4370add5950dd64dbf3e618925d13820f8eded
mendy hershkop <hers.mm@gmail.com>
2025-10-27 12:48:30 +0200

    create print_status function

--- hangman/io.py
+++ hangman/io.py
@@ -1,7 +1,9 @@
 def prompt_guess() -> str:
-    letter=input("Enter a letter: ")
-    return letter
+    while True:
+        letter=input("Enter a letter: ")
+        if len(letter)==1:
+            return letter
 def print_status(state: dict) -> None:
-    pass
+    print(f'{state["display"]}\n word you guess {state['guessed']} \n guess you have {(state['max_tries']- state['wrong_guesses'])}')
 def print_result(state: dict) -> None:
     pass
